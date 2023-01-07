9bc6526e6183509f0082601b41ed94f8de938a9c
nikhal99 <120049013+nikhal99@users.noreply.github.com>
2023-01-07 22:16:23 +0530

    Add files via upload

---
+++ loops.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+import javax.sound.sampled.SourceDataLine;
+public class loops{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+
+        int num = 1;
+
+        while(num <= n){
+            //System.out.println(num);
+            sum = sum + num;
+            num++; 
+         }{
+            System.out.println(sum);
+         }
+
+
+
+        
+    } 
+}
